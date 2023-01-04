4cc6594e47dad55c35484fdb0f8b4ebdb8f61fab
Victoria Gullotti <victoriagullotti101@gmail.com>
2023-01-04 08:09:11 +0800

    Duplicates file

---
+++ duplicates.py
@@ -0,0 +1,36 @@
+#List of characters with duplicates
+
+import pdb
+
+some_list = ['a', 'b', 'c', 'd', 'a', 's', 'c']
+
+#Function returns character if duplicate present or ''
+def is_duplicate(char):
+    count = 0
+
+    for i, character in enumerate(some_list):
+        # pdb.set_trace()
+        # print(f'{char}  {character} {count}')
+        
+        if char == character:
+            count += 1
+    
+    if count > 1:
+        return True
+    else:
+        return False 
+
+list = '' #list of duplicates
+
+for i in some_list:
+    
+        if is_duplicate(i):
+            try:
+                list.index(i)
+                
+            except ValueError:
+                list += ' ' + i
+                continue;
+    
+
+print('List of duplicated items: ' + list)
