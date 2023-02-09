15bc4455a81129ae2f9b0c5aab079fad9c9b05c8
Victoria Gullotti <victoriagullotti101@gmail.com>
2023-02-09 10:26:14 +0800

    Email sender

--- emailer.py
+++ emailer.py
@@ -1,14 +1,29 @@
 from smtplib import SMTP
 import json
-import requests
 from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.utils import COMMASPACE
 
-file_url = 'https://dummyjson.com/products'
+def send_email(to, subject, body):
+    sender = 'myemail@gmail.com'
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
 
-get_file = requests.get(file_url)
+    # Gmail credentials
+    username = "myemail@gmail.com"
+    password = "!Q2w3e4r"
 
-config = list()
-json_file = get_file.json()
-config.append(json_filec)
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(sender, to, msg.as_string())
+    server.quit()
+
+
+# Get email list from a JSON file
+with open('email_list.json') as json_file:
+    email_list = json.load(json_file)
+
+for recipient in email_list:
+    send_email(recipient, 'Subject', 'Email text')
