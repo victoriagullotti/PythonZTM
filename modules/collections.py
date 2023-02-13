a10ec28251d6ec923012dbf3160f9f99901881b0
Victoria Gullotti <victoriagullotti101@gmail.com>
2023-02-13 14:24:56 +0800

    collections

---
+++ modules/collections.py
@@ -0,0 +1,7 @@
+#from collections import Counter, defaultdict, OrderedDict
+import collections
+
+li = [1,2,3,4,5,6, 7, 8, 8]
+text = 'some text here'
+print(collections.Counter(li))
+print(collections.Counter(text))
