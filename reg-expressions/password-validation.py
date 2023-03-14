59db9296ad3740a78aaa6f5198daebbdc9c25134
Victoria Gullotti <victoriagullotti101@gmail.com>
2023-03-14 13:56:12 +0800

    Jupyter notebook files

--- reg-expressions/password-validation.py
+++ reg-expressions/password-validation.py
@@ -1,14 +1,14 @@
 import re
 
 #Any letter followed by a dot and then a letter a or A/a!!!!  
-regex = r'?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\u0020-\u002f\u003A-\u0040\u005B-\u0060\u007B-\u007E]).{8,}'    
+pattern = re.compile("?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\u0020-\u002f\u003A-\u0040\u005B-\u0060\u007B-\u007E]).{8,}")
 
-input = input("Enter the password: ")
+password = 'Aa1234567'
 
 def check(password):
 
     try:
-        m = re.match(regex, password)
+        m = re.search(pattern, password)
         
         if(m == None):    
             print("Invalid Password")
@@ -17,4 +17,5 @@ def check(password):
     except:
         print("Invalid Password")
 
-check(input)
+
+check(password)
